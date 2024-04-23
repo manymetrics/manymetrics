05a4f7a15e0104d39172703bf9de657082632d30
henadzit <henadzi.tsaryk@gmail.com>
2024-04-23 16:52:36 +0200

    Enable CORS

--- aws/api.tf
+++ aws/api.tf
@@ -1,3 +1,57 @@
+resource "aws_api_gateway_rest_api" "this" {
+  name = "manymetrics-api-${random_string.random.result}"
+}
+
+resource "aws_iam_role" "this" {
+  name = "manymetrics-role-${random_string.random.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+        Effect = "Allow"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "this" {
+  name = "manymetrics-policy-${random_string.random.result}"
+  role = aws_iam_role.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "kinesis:PutRecord",
+          "kinesis:PutRecords",
+        ]
+        Resource = aws_kinesis_stream.this.arn
+        Effect   = "Allow"
+      },
+    ]
+  })
+}
+
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name  = "prod"
+  depends_on  = [aws_api_gateway_integration.events]
+
+  triggers = {
+    redeployment = timestamp()
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_api_gateway_resource" "events" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
@@ -111,4 +165,12 @@ resource "aws_api_gateway_integration_response" "events_400" {
   }
 
   depends_on = [aws_api_gateway_integration.events]
+}
+
+module "events_cors" {
+  source = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+
+  api_id          = aws_api_gateway_rest_api.this.id
+  api_resource_id = aws_api_gateway_resource.events.id
 }
