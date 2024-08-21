fad71af46dae5e185dcc3fd3691ad46aa7b1ea4f
henadzit <henadzi.tsaryk@gmail.com>
2024-08-21 21:58:27 +0200

    Prettify the API lambda a bit

--- lambda/api_lambda/lambda_handler.py
+++ lambda/api_lambda/lambda_handler.py
@@ -1,8 +1,10 @@
-from datetime import datetime
-import json
 import os
-from typing import Any
+import json
 import traceback
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from functools import wraps
+from typing import Any
 
 import boto3
 
@@ -12,16 +14,39 @@ KINESIS_STREAM_NAME = os.environ.get("KINESIS_STREAM_NAME")
 kinesis_client = boto3.client("kinesis")
 
 
-def lambda_handler(lambda_event, context):
+def log_response(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+        print("Response: " + str(response))
+        return response
+
+    return wrapper
+
+
+@dataclass(frozen=True, kw_only=True)
+class Response:
+    statusCode: int
+    headers: dict[str, str]
+    body: dict[str, Any] | None = None
+
+    def to_lambda_response(self) -> dict[str, Any]:
+        lambda_response = asdict(self)
+        if self.body is not None:
+            lambda_response["body"] = json.dumps(self.body)
+        return lambda_response
+
+
+@log_response
+def lambda_handler(lambda_event, context) -> dict[str, Any]:
+    return _handle_event(lambda_event).to_lambda_response()
+
+
+def _handle_event(lambda_event: dict[str, Any]) -> Response:
     print("Received lambda_event: " + json.dumps(lambda_event))
 
     if _is_cors_request(lambda_event):
-        return _log_response(
-            {
-                "statusCode": 200,
-                "headers": _get_cors_headers(lambda_event),
-            }
-        )
+        return Response(statusCode=200, headers=_get_cors_headers(lambda_event))
 
     response_headers = {
         "Content-Type": "application/json",
@@ -29,23 +54,19 @@ def lambda_handler(lambda_event, context):
     }
 
     if not _validate_lambda_event(lambda_event):
-        return _log_response(
-            {
-                "statusCode": 400,
-                "headers": response_headers,
-                "body": json.dumps({"message": "Bad Request"}),
-            }
+        return Response(
+            statusCode=400,
+            headers=response_headers,
+            body={"message": "Bad Request"},
         )
 
     try:
-        event = _build_event(lambda_event)
+        event = _extract_and_enrich_event(lambda_event)
     except json.JSONDecodeError as e:
-        return _log_response(
-            {
-                "statusCode": 400,
-                "headers": response_headers,
-                "body": json.dumps({"message": "Bad Request"}),
-            }
+        return Response(
+            statusCode=400,
+            headers=response_headers,
+            body={"message": "Bad Request"},
         )
 
     try:
@@ -55,30 +76,22 @@ def lambda_handler(lambda_event, context):
             PartitionKey="partition_key",
         )
         print("Response from Kinesis: " + json.dumps(response))
-        return _log_response(
-            {
-                "statusCode": 200,
-                "headers": response_headers,
-                "body": json.dumps(
-                    {
-                        "message": "Ok",
-                    }
-                ),
-            }
+        return Response(
+            statusCode=200,
+            headers=response_headers,
+            body={
+                "message": "Ok",
+            },
         )
     except Exception as e:
         traceback.print_exception(e)
-        return _log_response(
-            {
-                "statusCode": 500,
-                "headers": response_headers,
-                "body": json.dumps(
-                    {
-                        "message": "Failed to send data to Kinesis stream",
-                        "error": str(e),
-                    }
-                ),
-            }
+        return Response(
+            statusCode=500,
+            headers=response_headers,
+            body={
+                "message": "Failed to send data to Kinesis stream",
+                "error": str(e),
+            },
         )
 
 
@@ -113,12 +126,7 @@ def _validate_lambda_event(lambda_event: dict[str, Any]) -> bool:
     return True
 
 
-def _log_response(response: dict[str, Any]) -> dict[str, Any]:
-    print("Response: " + json.dumps(response))
-    return response
-
-
-def _build_event(lambda_event: dict[str, Any]) -> dict[str, Any]:
+def _extract_and_enrich_event(lambda_event: dict[str, Any]) -> dict[str, Any]:
     event = json.loads(lambda_event["body"])
 
     event["server_event_time"] = (
